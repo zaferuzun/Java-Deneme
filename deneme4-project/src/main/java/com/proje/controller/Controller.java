cf7a49fec5c7bde2c40d886403ad204eb50cf1fe
zaferuzun <uznzafer@gmail.com>
2020-09-24 15:50:17 +0300

    first commit

---
+++ deneme4-project/src/main/java/com/proje/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.proje.controller;
+
+import com.proje.database.Repo;
+
+public class Controller {
+
+	public static void main(String[] args)
+	{
+		Repo repo = new Repo();
+		repo.Hello();
+	}
+}
